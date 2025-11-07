7fb7e78e32ee01698e80166e054df69d50447fc9
Felipe <12felipexd@gmail.com>
2025-11-06 22:34:51 -0300

    '06/11/2025'

--- CadastroProdutos/main.c
+++ CadastroProdutos/main.c
@@ -5,16 +5,29 @@
 typedef struct {
 	char nome[30];
 	float preco;
-	int estoque
+	int estoque;
 }Produtos;
 int main(int argc, char *argv[]) {
 	Produtos produto;
 	int choice;
 	do {
 		
-		printf("1 - Cadastrar produto\n 2 - Listar produtos cadastrados\n 3 - Buscar produto pelo nome\n 4 - Atualizar estoque de um produto\n 5 - Sair");
-		break;
+		printf("1 - Cadastrar produto\n 2 - Listar produtos cadastrados\n 3 - Buscar produto pelo nome\n 4 - Atualizar estoque de um produto\n 5 - Sair \n");
+		scanf("%d", &choice);
 		
+		switch (choice){
+			case 1:
+			 
+				printf("Nome do produto: ");
+				scanf("%s", produto.nome);
+				printf("Digite o preco do produto: ");
+				scanf("%f", &produto.preco);
+				printf("Digite a quantidade no estoque: ");
+				scanf("%d", &produto.estoque);
+				break;
+			
+			
+		}
 	}while(choice != 3);
 	
 	
